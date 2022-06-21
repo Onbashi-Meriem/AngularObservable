f8812d33a676e1c7c0170d304a3bedc718272cf5
Onbashi-Meriem <meryemonbasi5@gmail.com>
2022-06-21 11:07:56 +0200

    subscription

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.subscription = interval(1000).subscribe((res) => {
+      console.log(res);
+    });
   }
-
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
